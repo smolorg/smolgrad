d8e4cea1e1d6b624e3ec0111860d70d97b9d42e7
Maharshi Pandya <maharshipandya@192.168.1.5>
2024-05-30 19:01:23 +0530

    check output with torch

--- test_backward.py
+++ test_backward.py
@@ -3,14 +3,15 @@ from smolgrad.core import Tensor
 a = Tensor([[1, 2, 3], [4, 5, 6]], requires_grad=True)
 b = Tensor([[1, 2, 3], [4, 5, 6]], requires_grad=True)
 
-c = a.cat(b, axis=1)
-d = c + c
-e = d.log().sum()
+c = a + b
+d = a - b
+d = c * d
+e = d.sum()
 
 e.backward()
 
 print(a.grad, b.grad)
 
-# array([[1, 0.5, 0.333333],
-# [0.25, 0.2, 0.166667]], dtype=float32) array([[1, 0.5, 0.333333],
-# [0.25, 0.2, 0.166667]], dtype=float32)
+# array([[2, 4, 6],
+# [8, 10, 12]], dtype=float32) array([[-2, -4, -6],
+# [-8, -10, -12]], dtype=float32)
