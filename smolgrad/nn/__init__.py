ebe1af9ab0bfa63acdac3b6fc58cf50ec27be6f2
Maharshi Pandya <maharshipandya@192.168.1.5>
2024-06-20 11:04:10 +0530

    causal attention

--- smolgrad/nn/__init__.py
+++ smolgrad/nn/__init__.py
@@ -4,3 +4,4 @@ from ._losses import *
 
 from .linear import Linear
 from .sequential import Sequential
+from .dropout import Dropout
--- smolgrad/nn/attention.py
+++ smolgrad/nn/attention.py
@@ -4,20 +4,30 @@ from typing import *
 from smolgrad import Tensor
 from ._module import Module
 from .linear import Linear
+from .dropout import Dropout
 from ._activations import softmax
 
 
-class SelfAttention(Module):
+class CausalSelfAttention(Module):
     def __init__(
             self, context_size: int, d_embed: int, 
-            n_heads: int, device: str = "gpu"
+            n_heads: int, attn_pdrop: float = 0.7,
+            resd_pdrop: float = 0.6, device: str = "gpu"
         ) -> None:
         # TODO: dropout and attention mask
         super().__init__(device)
         self.use_np = False if device=="gpu" else True
+        self.context_size = context_size
 
-        self.w_proj = Linear(d_embed, 3 * d_embed)
-        self.o_proj = Linear(d_embed, d_embed)
+        self.w_proj = Linear(d_embed, 3 * d_embed, device=self.device)
+        self.o_proj = Linear(d_embed, d_embed, device=self.device)
+        self.attn_drop = Dropout(p=attn_pdrop, device=self.device)
+        self.resd_drop = Dropout(p=resd_pdrop, device=self.device)
+
+        self.mask = self._d.tril(
+            self._d.ones((context_size, context_size))
+        )
+        self.mask = self._d.reshape(self.mask, (1, 1, context_size, context_size))
         self.n_heads = n_heads
         self.d_embed = d_embed
 
@@ -30,13 +40,15 @@ class SelfAttention(Module):
 
         kT: Tensor = k.T(axes=[-1, -2])
         attn: Tensor = (q @ kT) * (1 / math.sqrt(k.shape[-1]))
+        _mask = (self.mask == 0).astype(self._d.int8)
+        attn = attn.masked_fill(_mask, float("-inf"))
         attn = softmax(attn, axis=-1)
+        attn = self.attn_drop(attn)
 
-        # TODO: add dropout and attention mask
         y = attn @ v
         y = y.T(axes=[1, 2]).reshape(bs, sl, ed)
         y = self.o_proj(y)
-
+        y = self.resd_drop(y)
         return y
 
 
