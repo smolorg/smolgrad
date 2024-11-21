b4202e5b1c7e9f4353363d36518b10fa8461dbf7
Maharshi Pandya <maharshipandya@192.168.1.5>
2024-11-22 04:13:41 +0530

    simulate ddp training 1 step

--- smolgrad/distributed/__init__.py
+++ smolgrad/distributed/__init__.py
@@ -1,1 +1,2 @@
 from .ddp import DistributedDataParallel
+from mpi4py import MPI as DDPCOMM
--- test_dist_model.py
+++ test_dist_model.py
@@ -1,34 +1,34 @@
+import smolgrad
 import numpy as np
-from mpi4py import MPI
 
-import smolgrad.nn as nn
-from smolgrad import Tensor
-from smolgrad.optims import SGD
-from smolgrad.distributed import DistributedDataParallel as DDP
+import smolgrad.distributed
 
-ROOT_PROCESS_ID = 0
-comm = MPI.COMM_WORLD
-model = nn.Sequential(
-    nn.Linear(2, 5),
-    nn.ReLU(),
-    nn.Linear(5, 10),
-    nn.ReLU(),
-    nn.Linear(10, 1),
-    nn.Sigmoid()
+
+learning_rate = 0.001
+
+comm = smolgrad.distributed.DDPCOMM.COMM_WORLD
+model = smolgrad.nn.Sequential(
+    smolgrad.nn.Linear(2, 5),
+    smolgrad.nn.ReLU(),
+    smolgrad.nn.Linear(5, 10),
+    smolgrad.nn.ReLU(),
+    smolgrad.nn.Linear(10, 1),
+    smolgrad.nn.Sigmoid()
 )
-lr = 0.001
-ddp_model = DDP(model, comm=comm, root=ROOT_PROCESS_ID)
-optimizer = SGD(ddp_model.parameters(), lr)
+ddp_model = smolgrad.distributed.DistributedDataParallel(model, comm=comm, root=0)
+optimizer = smolgrad.optims.SGD(ddp_model.parameters(), learning_rate)
+
 ddp_model.train()
 
-# simulates 1 step of training, 1 micro_batch
-micro_batch = Tensor(np.random.normal(size=(4, 2)).astype(np.float32))
+micro_batch = smolgrad.Tensor(np.random.normal(size=(4, 2)).astype(np.float32))
 output = ddp_model(micro_batch)
-loss = output.sum()
+loss = output.sum()     # dummy loss
 optimizer.zero_grad()
 loss.backward()
 ddp_model.synchronize()
 optimizer.step()
 
+# After the step, the parameters of the model
+# should be the same on all processes
 print(f"Rank {comm.rank}:")
-print(f"Last param's gradient: {ddp_model.parameters()[-1].grad}")
+print(f"Last parameter value: {ddp_model.parameters()[-1]}")
